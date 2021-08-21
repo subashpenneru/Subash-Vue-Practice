cc78e73143581dd0c0cb470b62e4f22fef3ced4a
Subash Penneru <subashpenneru@gmail.com>
2021-08-21 18:57:43 +0530

    basics done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,88 @@
 <template>
-  <div>App</div>
+  <section class="container">
+    <user-data :firstName="firstName" :lastName="lastName"></user-data>
+    <p>{{ uName }}</p>
+    <button @click="setNewAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInp" />
+      <button @click="setLastName">Set LastName</button>
+    </div>
+  </section>
 </template>
 
 <script>
+import { reactive, ref, computed, watch, provide } from 'vue';
+
+import UserData from './components/UserData.vue';
+
 export default {
-  name: 'App',
+  components: {
+    UserData
+  },
+  setup() {
+    /**PROPERTIES */
+    const user = reactive({ name: 'Subash', age: 27 });
+    const firstName = ref('');
+    const lastName = ref('');
+    const lastNameInp = ref(null);
+    const uAge = ref(27);
+
+    /**FUNCTIONS */
+    const setNewAge = () => {
+      user.age = 32;
+      uAge.value = 32;
+    };
+    const setLastName = () => {
+      lastName.value = lastNameInp.value.value;
+    };
+
+    /**COMPUTED */
+    const uName = computed(() => `${firstName.value} ${lastName.value}`);
+
+    /**WATCH */
+    watch([uAge, uName], (newVal, oldVal) => {
+      console.log(oldVal, newVal);
+    });
+
+    /**PROVIDE */
+    provide('userAge', uAge);
+
+    return {
+      userName: user.name,
+      age: user.age,
+      user,
+      uAge,
+      setNewAge,
+      firstName,
+      lastName,
+      uName,
+      lastNameInp,
+      setLastName
+    };
+  }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated
+} from 'vue';
+
+export default {
+  props: ['firstName', 'lastName'],
+  setup(props) {
+    const userName = computed(() => `${props.firstName} ${props.lastName}`);
+    const age = inject('userAge');
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+    onMounted(() => {
+      console.log('onMounted');
+    });
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
+    return { userName, age };
+  }
+};
+</script>
