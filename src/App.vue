1069960b31f5ba55efe30821350d68c8605cde9a
Subash Penneru <subashpenneru@gmail.com>
2021-08-21 19:41:10 +0530

    migrating done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,67 @@
 <template>
-  <div>App</div>
+  <main>
+    <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
+    <projects-list :user="selectedUser"></projects-list>
+  </main>
 </template>
 
 <script>
+import { ref } from 'vue';
+
+import USER_DATA from './dummy-data.js';
+import UserList from './components/users/UserList.vue';
+import ProjectsList from './components/projects/ProjectsList.vue';
+
 export default {
-  name: 'App',
+  components: {
+    UserList,
+    ProjectsList
+  },
+  setup() {
+    const activeUsers = USER_DATA;
+    let selectedUser = ref(null);
+
+    const selectUser = id => {
+      selectedUser.value = activeUsers.find(user => user.id === id);
+    };
+
+    return { selectUser, selectedUser, activeUsers };
+  }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+
+main {
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #00006b;
+  background-color: transparent;
+  color: #00006b;
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+}
+button:hover,
+button:active {
+  background-color: #efefff;
+}
+
+button.selected {
+  background-color: #00006b;
+  color: white;
 }
 </style>
---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,78 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search
+      v-if="hasProjects"
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <ul v-if="hasProjects">
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+import { computed, ref, watch, toRefs } from 'vue';
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+  components: {
+    ProjectItem
+  },
+  props: ['user'],
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const availableProjects = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter(prj =>
+          prj.title.toLowerCase().includes(activeSearchTerm.value.toLowerCase())
+        );
+      }
+      return props.user.projects;
+    });
+
+    const hasProjects = computed(() => {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+
+    watch(enteredSearchTerm, val => {
+      setTimeout(() => {
+        if (val === enteredSearchTerm.value) {
+          activeSearchTerm.value = val;
+        }
+      }, 300);
+    });
+
+    const { user } = toRefs(props);
+
+    watch(user, () => {
+      enteredSearchTerm.value = '';
+    });
+
+    const updateSearch = val => {
+      enteredSearchTerm.value = val;
+    };
+
+    return { enteredSearchTerm, hasProjects, availableProjects, updateSearch };
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
