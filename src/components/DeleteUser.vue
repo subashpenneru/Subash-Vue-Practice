bbbcf14e66a2d7c7061b41d578ecbe63e6435644
Subash Penneru <subashpenneru@gmail.com>
2021-08-22 08:40:26 +0530

    mixins basics done

---
+++ src/components/DeleteUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert';
+
+export default {
+  components: {
+    UserAlert
+  },
+  mixins: [alertMixin],
+  data() {
+    return {
+      alertTitle: 'Delete user?'
+    };
+  }
+};
+</script>
